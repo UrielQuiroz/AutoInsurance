9e2c322f515eeb2aff5c293cc665e4fa1a969751
UrielQuiroz <uriel.quiroz187@gmail.com>
2022-01-12 16:51:55 -0600

    Mostrando el resumen

--- auto-insurance/src/components/Resumen.jsx
+++ auto-insurance/src/components/Resumen.jsx
@@ -1,4 +1,14 @@
-import React, { Fragment } from 'react'
+import React, { Fragment } from 'react';
+import { primerMayuscula } from '../helper';
+import styled from '@emotion/styled';
+
+const ContenedorResumen = styled.div`
+    padding: 1rem;
+    text-align: center;
+    background-color: #00838F;
+    color: #FFF;
+    margin-top: 1rem;
+`;
 
 const Resumen = ({datos}) => {
 
@@ -6,14 +16,14 @@ const Resumen = ({datos}) => {
     if ([marca, year, plan].includes('')) return null; 
 
     return (
-        <Fragment>
+        <ContenedorResumen>
             <h2>Resumen de cotización</h2>
             <ul>
-                <li>Marca: </li>
-                <li>Plan: </li>
-                <li>Año del auto: </li>
+                <li>Marca: {primerMayuscula(marca)}</li>
+                <li>Plan: {primerMayuscula(plan)}</li>
+                <li>Año del auto: {year}</li>
             </ul>
-        </Fragment>
+        </ContenedorResumen>
     )
 }
 
