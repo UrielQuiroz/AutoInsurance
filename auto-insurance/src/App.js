9fd40f681a8166482026adca66861ebb4df1aed7
UrielQuiroz <uriel.quiroz187@gmail.com>
2022-01-12 12:16:21 -0600

    Styled Components
    Creando contenedor mediante styled components

--- auto-insurance/src/App.js
+++ auto-insurance/src/App.js
@@ -1,10 +1,19 @@
 import Header from "./components/Header";
+import styled from "@emotion/styled";
+
+const Contenedor = styled.div`
+  max-width: 600px;
+  margin: 0 auto;
+`;
+
 
 
 function App() {
   return (
-    <Header 
-      titulo='Cotizador de Seguros'/>
+    <Contenedor>
+            <Header 
+                titulo='Cotizador de Seguros'/>
+    </Contenedor>
   );
 }
 
